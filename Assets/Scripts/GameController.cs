cdad1c885bb96cdb741be7ca72badeb2f82d2db3
Robert Mroch <Robert.Mroch@Epsilon.com>
2016-02-26 13:58:12 -0600

    Add files to github

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject popcorn;
+    public Text timerText;
+    public float timeLeft;
+    public GameObject splashScreen;
+    public GameObject startButton;
+    public GameObject gameOver;
+
+    private bool playing;
+    private float maxWidth;
+
+    void Start()
+    {
+        splashScreen.SetActive(true);
+        startButton.SetActive(true);
+        gameOver.SetActive(false);
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float popcornWidth = popcorn.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - popcornWidth;
+        UpdateText();
+        playing = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (playing)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+                gameOver.SetActive(true);
+            }
+            UpdateText();
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        while (timeLeft > 0)
+        {
+            int popSpawnQuantity = Random.Range(2, 7);
+            while (popSpawnQuantity > 0)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y + Random.Range(1, 5), 0.0f);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(popcorn, spawnPosition, spawnRotation);
+                popSpawnQuantity--;
+            }
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.0f));
+        }
+    }
+
+    void UpdateText()
+    {
+        timerText.text = string.Format("Time Left:\n{0}", Mathf.RoundToInt(timeLeft));
+    }
+
+    public void StartGame()
+    {
+        splashScreen.SetActive(false);
+        startButton.SetActive(false);
+        StartCoroutine(Spawn());
+        playing = true;
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour
+{
+
+    public Text scoreText;
+    public int zonkerValue;
+
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = 0;
+
+    }
+
+    void OnTriggerEnter2D()
+    {
+        score += zonkerValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = string.Format("Zonkers:\n{0}", score);
+    }
+}
